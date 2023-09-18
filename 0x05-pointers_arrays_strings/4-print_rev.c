97ac6b915d41bd053db632da86ce35b50c135e3b
Soid23 <dorrismutahi@gmail.com>
2023-09-18 05:37:02 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev -  prints a string, in reverse followed by a new line.
+ *
+ * @s : string to print
+ */
+void print_rev(char *s);
+{
+	int i, k, len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (k = len - 1; k >= 0; k--)
+	{
+		putchar(s[j]);
+	}
+
+	 putchar('\n');
+}
+
