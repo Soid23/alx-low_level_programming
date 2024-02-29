6598de64936fa00d620b6ca243400794f4ab6e8f
Soid23 <dorrismutahi@gmail.com>
2024-02-28 19:23:40 -0800

    1000 c file

---
+++ x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - checks memory allocated using malloc
+ * @b : bytes to be allocated
+ * Return : value of pointer
+ */
+
+void *malloc_checked(unsigned int)
+{
+	void *f;
+	f = malloc 
